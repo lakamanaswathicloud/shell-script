d2595d17aa9734a5eb448754d83c3df14cec80a9
lakamanaswathicloud <lakamanaswathi2021.com>
2024-06-17 17:18:30 +0530

    08,09-data-types.sh added

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -12,9 +12,9 @@ NUMBER1=$1
 
 if [ $NUMBER1 -lt 10 ]
 then
-    echo "Given number $NUMBER1 is greater than 10"
+    echo "Given number $NUMBER1 is less than 10"
 else
-    echo "Given number $NUMBER1 is less than 10" 
+    echo "Given number $NUMBER1 is greater than 10" 
 fi
 
 #-gt, -lt, -eq, -ge, -le
