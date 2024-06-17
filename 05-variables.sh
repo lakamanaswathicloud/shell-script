ec6820ea2463b57e146cdd7ebdd693500a702cd9
lakamanaswathicloud <lakamanaswathi2021.com>
2024-06-17 16:19:54 +0530

    05-varibales.sh added

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,1 +1,11 @@
-This is a sample script
+#!/bin/bash
+
+echo "Plese enter userName::"
+
+read  -S USERNAME #here USERNAME is variable
+
+echo "Plese enter PASSWORD::"
+
+read  -S USERNPASSWORDAME #here PASSWORD is variable
+
+
