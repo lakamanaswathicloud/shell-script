a24bb35bb60d63a8c2e2a2727e4e26cd44ef42c9
lakamanaswathicloud <lakamanaswathi2021.com>
2024-06-17 16:29:10 +0530

    06-varibales.sh added

--- 06-ayyays.sh
+++ 06-ayyays.sh
@@ -1,1 +1,18 @@
-This is a sample script
+#!/bin/bash
+
+MOVIES =("RRR" "DJTILLU "MURARI")
+
+
+# size of above array is 3 
+
+
+# index are 0,1,2 
+
+#list always start with 0.
+
+
+echo "First Movie is :"${MOVIES[0]}
+echo "Second Movie is :"$MOVIES[1]}
+
+echo "Third Movie is :"$MOVIES[2]}
+echo "Third Movie is :"$MOVIES[@]}
