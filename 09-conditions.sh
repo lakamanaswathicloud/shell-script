0ce67d0facee3df17f1b26e108e3f1735c1d6574
lakamanaswathicloud <lakamanaswathi2021.com>
2024-06-17 17:16:49 +0530

    09-conditions.sh added

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -8,5 +8,13 @@ then
 else
     echo "Given number $NUMBER is less than 10" 
 fi
+NUMBER1=$1
+
+if [ $NUMBER1 -lt 10 ]
+then
+    echo "Given number $NUMBER1 is greater than 10"
+else
+    echo "Given number $NUMBER1 is less than 10" 
+fi
 
 #-gt, -lt, -eq, -ge, -le
